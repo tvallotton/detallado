7e8ef40ac7286199afb0fb3d2d30de93ef4bb6cc
Tomás Vallotton <tvallotton@uc.cl>
2024-11-17 22:36:55 +0000

    more use of simple skill

--- Fire-Emblem/Model/Skills/SimpleSkill.cs
+++ Fire-Emblem/Model/Skills/SimpleSkill.cs
@@ -4,25 +4,33 @@ using Fire_Emblem;
 
 class SimpleSkill : BaseSkill {
 
-    private Effect[] _effect;
+    private Func<Game, int, Effect[]> _effect;
 
     public SimpleSkill(string name, BaseCondition condition, Effect[] playerEffect) {
         this.name = name;
         this.condition = condition;
-        _effect = playerEffect;
+        _effect = (_game, _player) => playerEffect;
     }
 
     public SimpleSkill(string name, BaseCondition condition, Effect playerEffect) {
         this.name = name;
         this.condition = condition;
-        _effect = [playerEffect];
+        _effect = (_game, _player) => [playerEffect];
     }
+
+    public SimpleSkill(string name, BaseCondition condition, Func<Game, int, Effect[]> playerEffects) {
+        this.name = name;
+        this.condition = condition;
+        _effect = playerEffects;
+    }
+
+
     public override string name { get; } = "Simple Skill";
 
     public override BaseCondition condition { get; } = new Always();
 
     public override IEnumerable<Effect> PlayerEffects(Game game, int player) {
-        return _effect;
+        return _effect(game, player);
     }
 
 }
--- Fire-Emblem/Model/Skills/Skill.cs
+++ Fire-Emblem/Model/Skills/Skill.cs
@@ -9,25 +9,152 @@ public class Skill {
     BaseSkill skill;
 
     static BaseSkill[] SKILLS = {
-        new ArmoredBlow(),
-        new AktDefPlus5(),
-        new AktResPlus5(),
-        new SpdResPlus5(),
-        new DefensePlus5(),
-        new ResistancePlus5(),
-        new AttackPlus6(),
-        new SpeedPlus5(),
+        new SimpleSkill(
+            "Armored Blow",
+            new OnPlayersTurn(),
+            new Effect {
+                difference = new Stats<int> {
+                    Def = 8
+                }
+        }),
+        new SimpleSkill(
+            "Atk/Def +5",
+            new Always(),
+            new Effect {
+                difference = new Stats<int> {
+                    Def = 5,
+                    Atk = 5
+                }
+        }),
+        new SimpleSkill(
+            "Atk/Res +5",
+            new Always(),
+            new Effect {
+                difference = new Stats<int> {
+                    Res = 5,
+                    Atk = 5
+                }
+        }),
+        new SimpleSkill(
+            "Spd/Res +5",
+            new Always(),
+            new Effect {
+            difference = new Stats<int> {
+                Spd = 5,
+                Res = 5,
+            }
+        }),
+        new SimpleSkill(
+            "Defense +5",
+            new Always(),
+            new Effect {
+                    difference = new Stats<int> {
+                Def = 5
+            }
+        }),
+        new SimpleSkill(
+            "Resistance +5",
+            new Always(),
+            new Effect {
+            difference = new Stats<int> {
+                Res = 5
+            }
+        }),
+        new SimpleSkill(
+            "Attack +6",
+            new Always(),
+            new Effect {
+            difference = new Stats<int> {
+                Atk = 6
+            }
+        }),
+        
         new FairFight(),
-        new BracingBlow(),
-        new BrazenAtkSpd(),
-        new BrazenAtkDef(),
-        new BrazenAtkRes(),
-        new BrazenSpdDef(),
-        new BrazenSpdRes(),
-        new BrazenDefRes(),
-        new LifeAndDeath(),
-        new FortDefRef(),
-        new StillWater(),
+        
+        new SimpleSkill(
+            "Bracing Blow",
+            new OnPlayersTurn(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Def = 6,
+                        Res = 6
+                    }
+                }
+        ),
+        
+        
+
+        new SimpleSkill(
+            "Brazen Atk/Res",
+            new OnPlayerLowHP(80),
+            new Effect {
+                    difference = new Stats<int> {
+                        Res = 10,
+                        Atk = 10
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Brazen Atk/Def",
+            new OnPlayerLowHP(80),
+            new Effect {
+                    difference = new Stats<int> {
+                        Def = 10,
+                        Atk = 10
+                    }
+                }
+        ),
+
+        new SimpleSkill(
+            "Brazen Atk/Spd",
+            new OnPlayerLowHP(80),
+            new Effect {
+                    difference = new Stats<int> {
+                        Spd = 10,
+                        Atk = 10
+                    }
+                }
+        ),    
+        new SimpleSkill(
+            "Brazen Spd/Res",
+            new OnPlayerLowHP(80),
+            new Effect {
+                    difference = new Stats<int> {
+                        Res = 10,
+                        Spd = 10
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Brazen Spd/Def",
+            new OnPlayerLowHP(80),
+            new Effect {
+                    difference = new Stats<int> {
+                        Def = 10,
+                        Spd = 10
+                    }
+                }
+        ), 
+        new SimpleSkill(
+            "Brazen Def/Res",
+            new OnPlayerLowHP(80),
+            new Effect {
+                    difference = new Stats<int> {
+                        Res = 10,
+                        Def = 10
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Brazen Atk/Spd",
+            new OnPlayerLowHP(80),
+            new Effect {
+                    difference = new Stats<int> {
+                        Spd = 10,
+                        Atk = 10
+                    }
+                }
+        ),
         new Resolve(),
         new SwiftSparrow(),
         new CloseDef(),
@@ -39,31 +166,242 @@ public class Skill {
         new LullDefRes(),
         new LightAndDark(),
         new LullSpdDef(),
-        new SwordAgility(),
         new BeorcsBlessing(),
-        new AgneasArrow(),
+        new SimpleSkill(
+            "Agnea's Arrow",
+            new Always(),
+            new Effect {
+                    neutralizedPenalty = Stats<bool>.All(),
+                }
+        ),
         new Dragonskin(),
-        new SwordFocus(),
-        new SwordPower(),
-        new LancePower(),
-        new LanceAgility(),
-        new AxePower(),
-        new BowAgility(),
-        new BowFocus(),
-        new SolidGround(),
-        new SturdyBow(),
-        new EarthBoost(),
-        new DeadlyBlade(),
+        
+        new SimpleSkill(
+            "Sword Power",
+            new OnPlayerWeapon(Weapon.Sword),
+            new Effect {
+                    difference = new Stats<int> {
+                        Def = -10,
+                        Atk = 10
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Sword Agility",
+            new OnPlayerWeapon(Weapon.Sword),
+            new Effect {
+                    difference = new Stats<int> {
+                        Spd = 12,
+                        Atk = -6
+                    }
+                }
+        ),
+
+        new SimpleSkill(
+            "Sword Focus",
+            new OnPlayerWeapon(Weapon.Sword),
+            new Effect {
+                    difference = new Stats<int> {
+                        Res = -10,
+                        Atk = 10,
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Sturdy Blow",
+            new OnPlayersTurn(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Atk = 6,
+                        Def = 6,
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Still Water",
+            new Always(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Atk = 6,
+                        Res = 6,
+                        Def = -2
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Speed +5",
+            new Always(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Spd = 5,
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Spd/Res +5",
+            new Always(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Spd = 5,
+                        Res = 5,
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Resistance +5",
+            new Always(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Res = 5
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Perceptive",
+            new OnPlayersTurn(),
+            (game, player) => [new Effect {
+                difference = new Stats<int> {
+                    Spd = 12 + game.Fighter(player).Get(Stat.Spd) / 4,
+                }
+            }]
+        ),
+        new SimpleSkill(
+            "Solid Ground",
+            new Always(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Res = -5,
+                        Atk = +6,
+                        Def = +6,
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Life and Death",
+            new Always(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Atk = 6,
+                        Spd = 6,
+                        Def = -5,
+                        Res = -5
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Lance Power",
+            new OnPlayerWeapon(Weapon.Lance),
+            new Effect {
+                    difference = new Stats<int> {
+                        Def = -10,
+                        Atk = 10
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Lance Agility",
+            new OnPlayerWeapon(Weapon.Lance),
+            new Effect {
+                    difference = new Stats<int> {
+                        Spd = 12,
+                        Atk = -6
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Golden Lotus",
+            new OnRivalWeapon(Weapon.Magic).Not(),
+            new Effect {
+                    percentDamageReduction = 50,
+                    scope = Scope.FIRST_ATTACK
+                }
+        ),
+        new SimpleSkill(
+            "Fort. Def/Res",
+            new Always(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Atk = -2,
+                        Def = 6,
+                        Res = 6
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Earth Boost",
+            new OnHigherPlayerHP(byHowMuch: 3),
+            new Effect {
+                    difference = new Stats<int> {
+                        Def = 6,
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Defense +5",
+            new Always(),
+            new Effect {
+                    difference = new Stats<int> {
+                        Def = 5
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Deadly Blade",
+            new OnPlayerWeapon(Weapon.Sword),
+            new Effect {
+                    difference = new Stats<int> {
+                        Atk = 8,
+                        Spd = 8
+                    }
+                }
+        ),
+        new SimpleSkill(
+            "Axe Power",
+            new OnPlayerWeapon(Weapon.Axe),
+            new Effect {
+                    difference = new Stats<int> {
+                        Def = -10,
+                        Atk = 10
+                    }
+                }
+        ),
+        
+        new SimpleSkill(
+            "Bow Agility",
+            new OnPlayerWeapon(Weapon.Bow),
+            new Effect {
+                    difference = new Stats<int> {
+                        Spd = 12,
+                        Atk = -6
+                    }
+                }
+        ),
+        
+        new SimpleSkill(
+            "Bow Focus",
+            new OnPlayerWeapon(Weapon.Bow),
+            new Effect {
+                    difference = new Stats<int> {
+                        Res = -10,
+                        Atk = 10
+                    }
+                }
+        ),
         new Wrath(),
-        new SingleMinded(),
+        new SimpleSkill(
+            "Single-Minded",
+            new OnRivalIsLatestOpponent(),
+            new Effect {
+                difference = new Stats<int> {
+                    Atk = 8,
+                }
+            }
+        ),
         new Charmer(),
-        new Perceptive(),
         new Dodge(),
         new DragonWall(),
-        new GoldenLotus(),
         new AegisShield(),
         new BlueSkies(),
-        new BracingStance(),
         new Bushido(),
         new Chivalry(),
         new DartingStance(),
