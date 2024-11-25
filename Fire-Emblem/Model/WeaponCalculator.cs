82621cf00c05ad6106da438685f2b8b8ff336c7e
Tomás Vallotton <tvallotton@uc.cl>
2024-11-25 00:27:10 -0300

    move weaponcalculator

---
+++ Fire-Emblem/Model/WeaponCalculator.cs
@@ -0,0 +1,18 @@
+
+static class WeaponCalculator {
+    public static bool HasAdvantageOver(this Weapon first, Weapon second) {
+        return ((first == Weapon.Sword) && (second == Weapon.Axe))
+            || ((first == Weapon.Lance) && (second == Weapon.Sword))
+            || ((first == Weapon.Axe) && (second == Weapon.Lance));
+    }
+
+    public static double WTB(this Weapon player, Weapon rival) {
+        if (player.HasAdvantageOver(rival)) {
+            return 1.2;
+        } else if (rival.HasAdvantageOver(player)) {
+            return 0.8;
+        } else {
+            return 1;
+        }
+    }
+}
